de9b95b2c742491f0ea34664bf8240731028bfcb
ajayshirse <ajayshirse9665@gmail.com>
2026-08-27 23:55:05 +0530

    Create SumOfDigits Program

---
+++ DaysLogicalProgrammingChallenge/src/Day06/SumOfDigits.java
@@ -0,0 +1,18 @@
+package Day06;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Sum of Digits = " + sum);
+    }
+}
